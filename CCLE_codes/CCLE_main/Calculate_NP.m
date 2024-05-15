e4b29b95b1018058f223f74358265aced382201b
bigpaul97 <50006016+bigpaul97@users.noreply.github.com>
2024-05-15 18:10:05 -0400

    Delete CCLE_codes directory

--- CCLE_codes/CCLE_main/Calculate_NP.m
+++
@@ -1,9 +0,0 @@
-function [NP,Anc] = Calculate_NP(loop_parent,Loop_order,l_foot,r_foot,PT,NP,N)
-
-Anc(1) = PT;
-Anc(2) = PT;
-while loop_parent
-    [NP,Anc,loop_parent] = Calculate_SingleNP(loop_parent,Loop_order,l_foot,r_foot,Anc,NP,N);
-end
-
-end
